9f9d0718138c2685231967fd89d1f89a274b3f36
Danny Hadley <dadleyy@gmail.com>
2016-10-16 11:30:47 -0400

    stubbing reducer test suite

---
+++ test/unit/reducers.spec.js
@@ -0,0 +1,15 @@
+const reducers = require("../../src/reducers");
+
+describe("reducer test suite", function() {
+
+  describe("sorting reducer", function() {
+
+    it("should update rel if column_click", function() {
+    });
+
+    it("should update order if column_click on same rel", function() {
+    });
+
+  });
+
+});
